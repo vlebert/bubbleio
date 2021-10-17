9733c7d182eb71d7b020d3f279020871139c546c
vlebert <49779177+vlebert@users.noreply.github.com>
2021-10-17 14:28:34 +0200

    better setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,20 @@ from setuptools import find_packages, setup
 
 # package (to get version)
 from bubbleio import __about__
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="bubbleio",
     version=__about__.__version__,
     author=__about__.__author__,
     author_email=__about__.__email__,
-    description=__about__.__summary__,
-    py_modules=["sqlitetoairtable"],
+    url="https://github.com/vlebert/bubbleio",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["bubbleio"],
     # packaging
     packages=find_packages(
         exclude=["contrib", "docs", "*.tests", "*.tests.*", "tests.*", "tests"]
