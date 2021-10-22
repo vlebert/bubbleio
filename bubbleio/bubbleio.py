f9c8c329999ac34527f282bce3b58cdf6e36b43f
vlebert <49779177+vlebert@users.noreply.github.com>
2021-10-22 09:30:25 +0200

    simpler pandas function

--- bubbleio/bubbleio.py
+++ bubbleio/bubbleio.py
@@ -77,7 +77,7 @@ class Bubbleio:
             >>> API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
             >>> API_ROOT = "https://appname.bubbleapps.io/api/1.1/obj"
             >>> bbio = Bubbleio(API_KEY, API_ROOT)
-            >>> bbio.get("foo_type")
+            >>> bbio.get("fooType")
             {
                 "cursor": 0,
                 "results": [
@@ -131,7 +131,7 @@ class Bubbleio:
             >>> API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
             >>> API_ROOT = "https://appname.bubbleapps.io/api/1.1/obj"
             >>> bbio = Bubbleio(API_KEY, API_ROOT)
-            >>> bbio.get_results("foo_type")
+            >>> bbio.get_results("fooType")
             [
                 {
                     "foo_field_1": "value",
@@ -179,9 +179,7 @@ class Bubbleio:
 
         return records
 
-    def get_results_as_df(
-        self, typename, limit=None, cursor=None, list_fields=None, mask_fields=None
-    ):
+    def get_results_as_df(self, typename, limit=None, cursor=None):
         """Returns results as a Pandas.DataFrame
 
         Args:
@@ -189,107 +187,70 @@ class Bubbleio:
             limit (str): Use `limit` to specify the number of items you want in the response.
                          The default and maximum allowed is 100. Use cursor to specify where to start.
             cursor (str): This is the rank of the first item in the list.
-            list_fields(list): The list of fields to returns in the DataFrame. All other fields will
-                               be removed from the DataFrame. If None, all fields are returned.
-                               Cant't be use in combination with mask_fields.
-            mask_fields(list): The list of fields to remove from the DataFrame.
-                               Cant't be use in combination with mask_fields.
-
-        Returns:
-            Pandas.DataFrame: The list of all items of the type.
-        """
-        if list_fields and mask_fields:
-            raise ValueError(
-                "get_results_as_df(): list_fields and mask_fields can't be used at the same time"
-            )
-        else:
-            df = pd.DataFrame(self.get_results(typename, limit=limit, cursor=cursor))
-            if list_fields:
-                df = df.loc[:, df.columns.isin(list_fields)]
-            elif mask_fields:
-                df = df.loc[:, ~df.columns.isin(mask_fields)]
-        return df
-
-    def get_all_results_as_df(
-        self, typename, list_fields=None, mask_fields=None, joins=None, drop_id=True
-    ):
-        """Returns all results as a Pandas.DataFrame
-
-        Args:
-            typename (str): The type of "things" you are querying..
-            list_fields(list): The list of fields to returns in the DataFrame. All other fields will
-                               be removed from the DataFrame. If None, all fields are returned. This
-                               option also allow to order the columns.
-                               Cant't be use in combination with mask_fields.
-            mask_fields(list): The list of fields to remove from the DataFrame.
-                               Cant't be use in combination with mask_fields.
-            joins(list): List of dicts giving the parameters of the joins to be made. Dict must have
-                         these Items :
-
-                         - field: Name of the field referencing the foreign table (foreign key)
-                         - typename: Name of the foreign table to be joined
-                         - list_fields: same logic as above
-                         - mask_fields: same logic as above
-            drop_id(boolean): If true, the column of the _id of foreign table will be dropped
-                              (columns corresponding to "field" item in "joins")
 
         Returns:
             Pandas.DataFrame: The list of all items of the type.
 
         Examples:
 
+            >>> from bubbleio import Bubbleio
+            >>> bbio = Bubbleio(API_KEY, API_ROOT)
+            >>> bbio.get_results("fooType")
+            _id     fooField1   fooField2   fooBar
+            idFoo1  value       value       idBar1
+            idFoo2  value       value       idBar2
+        """
+        df = pd.DataFrame(self.get_results(typename, limit=limit, cursor=cursor))
+        return df
+
+    def get_all_results_as_df(self, typename, joins=None):
+        """Returns all results as a Pandas.DataFrame
+
+        Args:
+            typename (str): The type of "things" you are querying..
+            joins(list): List of dicts giving the parameters of the joins to be made. Dict must have
+                         these Items :
+
+                         - field: Name of the field referencing the foreign table (foreign key)
+                         - typename: Name of the foreign table to be joined
+
+        Returns:
+            Pandas.DataFrame: The list of all items of the type.
+
+        Examples:
+
+            >>> from bubbleio import Bubbleio
+            >>> bbio = Bubbleio(API_KEY, API_ROOT)
             >>> joins_ = [
             ...     {
-            ...         "field": "Bar",
-            ...         "typename": "bar",
+            ...         "field": "fooBar",
+            ...         "typename": "barType",
             ...     }
             ... ]
-            >>> bbio.get_all_results_as_df("foo", joins=joins_, drop_id=False)
-            _id     fooField1   fooField2   Bar     Bar_barField1   Bar_barField2
-            idFoo1  value       value       idBar1  value           value
-            idFoo2  value       value       idBar2  value           value
+            >>> bbio.get_results("fooType")
+            _id     fooField1   fooField2   fooBar
+            idFoo1  value       value       idBar1
+            idFoo2  value       value       idBar2
+            >>> bbio.get_results("barType")
+            _id     barField1   barField2
+            idBar1  value       value
+            idBar2  value       value
+            >>> bbio.get_all_results_as_df("fooType", joins=joins_, drop_id=False)
+            _id     fooField1   fooField2   fooBar  fooBar__id      fooBar_barField1   fooBar_barField2
+            idFoo1  value       value       idBar1  idBar1          value              value
+            idFoo2  value       value       idBar2  idBar2          value              value
         """
-        if list_fields and mask_fields:
-            raise ValueError(
-                "get_all_results_as_df(): list_fields and mask_fields can't be used at the same time"
-            )
-        else:
-            df = pd.DataFrame(self.get_all_results(typename))
-            if list_fields:
-                df = df[list_fields]
-            elif mask_fields:
-                df = df.loc[:, ~df.columns.isin(mask_fields)]
-
-            if joins:
-                for j_param in joins:
-                    # _id must be keeped to allow joins based on this key
-                    list_fields_ = j_param.get("list_fields")
-                    mask_fields_ = j_param.get("mask_fields")
-                    # So we append "_id" if list_fields is defined and does not contain "_id"
-                    if list_fields_:
-                        list_fields_.append(
-                            "_id"
-                        ) if "_id" not in list_fields_ else list_fields_
-                    # Or we remove it from mask_fields if needed
-                    if mask_fields_:
-                        mask_fields_.remove(
-                            "_id"
-                        ) if "id" in mask_fields_ else mask_fields_
-                    foreign_table = self.get_all_results_as_df(
-                        j_param["typename"],
-                        list_fields=list_fields_,
-                        mask_fields=mask_fields_,
-                    )
-                    # Add prefix to avoid confusion
-                    foreign_table = foreign_table.add_prefix(j_param["field"] + "_")
-                    df = df.merge(
-                        foreign_table,
-                        how="left",
-                        left_on=j_param["field"],
-                        right_on=j_param["field"] + "_id",
-                    )
-                    # We drop the _id of joined column
-                    df = df.drop(columns=[j_param["field"] + "_id"])
-                    if drop_id:
-                        df = df.drop(columns=[j_param["field"]])
+        df = pd.DataFrame(self.get_all_results(typename))
+        if joins:
+            for j_param in joins:
+                foreign_table = self.get_all_results_as_df(j_param["typename"])
+                # Add prefix to avoid confusion
+                prefix = j_param["field"] + "_"
+                foreign_table = foreign_table.add_prefix(prefix)
+                df = df.merge(
+                    foreign_table,
+                    how="left",
+                    left_on=j_param["field"],
+                    right_on=prefix + "_id",
+                )
         return df
